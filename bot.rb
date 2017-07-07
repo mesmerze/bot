91607b1369a7ef56c61f65a780465c9ed52a9f0e
mesmerze <aichernovmail@gmail.com>
2017-07-07 18:24:20 +0300

    bot itself

---
+++ bot.rb
@@ -0,0 +1,60 @@
+require 'telegram/bot'
+require './lib.rb'
+token = "439391885:AAFmePFsAZsMwsBPP7QDUsU0aflP-Yb2Bao"
+
+def mapping_results(data)
+  data.map do |arr|
+  Telegram::Bot::Types::InlineQueryResultArticle.new(
+    id: arr[0],
+    title: arr[1],
+    input_message_content: Telegram::Bot::Types::InputTextMessageContent.new(
+      message_text: arr[2]
+    )
+  )
+  end
+end
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+  case message
+  when Telegram::Bot::Types::InlineQuery
+    case message.query
+    when ''
+      results = mapping_results(@categories)
+    bot.api.answer_inline_query(inline_query_id: message.id, results: results)
+    when 'rubizza'
+      results = mapping_results(@rubizzas)
+    bot.api.answer_inline_query(inline_query_id: message.id, results: results)
+    when 'ruby'
+      results = mapping_results(@rubies)
+    bot.api.answer_inline_query(inline_query_id: message.id, results: results)
+    when 'stuff'
+      results = mapping_results(@stuff)
+    bot.api.answer_inline_query(inline_query_id: message.id, results: results)
+    end
+  when Telegram::Bot::Types::Message
+    case message.text
+    when '/start'
+      bot.api.sendVideo(chat_id: message.chat.id, video: "http://coub.com/view/uxngm")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.last_name}")
+    when '/rand'
+      bot.api.sendVideo(chat_id: message.chat.id, video: @top_gear[rand(8)])
+    when '/commands'
+      bot.api.send_message(chat_id: message.chat.id, text:
+      'Commands: /start, /stop, /rand, /commands')
+    else
+      if message.photo && message.sticker.nil?
+        bot.api.sendVideo(chat_id: message.chat.id, video:
+        'http://coub.com/view/1zv8z')
+      end
+      if message.sticker
+        bot.api.send_sticker(chat_id: message.chat.id, sticker:
+        'CAADBAADEQAD2uDCCfMwhYb-87y4Ag')
+      end
+      bot.api.send_message(chat_id: message.chat.id, text:
+      'Commands: /start, /stop, /rand, /commands')
+    end
+  end
+  end
+end
---
+++ lib.rb
@@ -0,0 +1,33 @@
+@categories = [
+  [1, 'rubizza', 'Org info. Please type category'],
+  [2, 'ruby', 'Ruby info. Please type category'],
+  [3, 'stuff', 'Another interesting here. Please type category']
+             ]
+@rubies = [
+    [1, 'Style guide', 'https://github.com/bbatsov/ruby-style-guide'],
+    [2, 'RuboCOp', 'https://github.com/bbatsov/rubocop'],
+    [3, 'Rbenv', 'http://octopress.org/docs/setup/rbenv/'],
+    [4, 'Rbenv-bundler', 'https://github.com/carsomyr/rbenv-bundler'],
+    [5, 'Telegram Gem', 'https://github.com/atipugin/telegram-bot-ruby']
+         ]
+@rubizzas = [
+  [1, 'Life journal', 'https://goo.gl/gj9Nku'],
+  [2, 'Achievements', 'https://goo.gl/hQccit'],
+  [3, 'Progects', 'https://goo.gl/yfGSrB'],
+  [4, 'Stuff', 'https://goo.gl/eieVMF']
+           ]
+@stuff = [
+    [1, 'Vim', 'http://www.vim.org/'],
+    [2, 'TelegramAPI', 'https://core.telegram.org/bots/api'],
+    [3, 'Koans', 'https://github.com/cybergizer/ruby_koans'],
+    [4, 'NeoVim', 'https://neovim.io/'],
+    [5, 'PR Github', 'http://www.dasblinkenlichten.com/how-to-create-a-github-pull-request-pr/']
+        ]
+@top_gear = %w(http://gph.is/2c2yAap
+             https://media.giphy.com/media/1hfsl1N228EZW/giphy.gif
+             https://media.giphy.com/media/8Mo1MsxB90i6k/giphy.gif
+             https://media.giphy.com/media/PVy0nN57k4q6A/giphy.gif
+             https://media.giphy.com/media/po6rUkWRQ3MtO/giphy.gif
+             https://media.giphy.com/media/pqPt14iFsg5KE/giphy.gif
+             https://media.giphy.com/media/LUbDVs4M7Rxba/giphy.gif
+             https://media.giphy.com/media/mHm5vSzMg3jNK/giphy.gif)
