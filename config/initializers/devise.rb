b279acc11708ce0f5e4e1e8a963dec6f15be4b39
Alexei Chernov <aichernovmail@gmail.com>
2018-04-03 19:48:26 +0300

    Changed google envs names (#137)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,7 +264,7 @@ Devise.setup do |config|
   # up on your models and hooks.
 
   require 'omniauth-google-oauth2'
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], access_type: 'offline', approval_prompt: '', scope: 'userinfo.email'
+  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH2_CLIENT_ID'], ENV['GOOGLE_OAUTH2_CLIENT_SECRET'], access_type: 'offline', approval_prompt: '', scope: 'userinfo.email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
