5b36e35d11da5442a642d7c276140861b1847e86
Alexei Chernov <aichernovmail@gmail.com>
2018-04-13 19:09:49 +0300

    Update rails and papertrail to fixed version, move activesupport to node (#159)
    modules

--- app/javascript/app/index.js
+++ app/javascript/app/index.js
@@ -56,7 +56,7 @@ import './js/shops_picker.coffee';
 import './js/meetings.coffee';
 import './js/systems.coffee';
 
-import * as ActiveStorage from 'javascripts/activestorage'
+import * as ActiveStorage from 'activestorage'
 import './js/direct_uploads';
 ActiveStorage.start();
 
